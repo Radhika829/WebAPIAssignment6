fab2ffe3cacfca6ddaedf5198ae370af0c6e184e
Radhika <radhikasn06@gmail.com>
2024-02-07 15:00:22 +0530

    committted

---
+++ WebAPIAssignement6/Data/MovieDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPIAssignement6.Models;
+
+namespace WebAPIAssignement6.Data
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext (DbContextOptions<MovieDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAPIAssignement6.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ WebAPIAssignement6/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPIAssignement6.Models
+{
+    [Table("Movie")]
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(200)]
+        public string MName { get; set; }
+        public string StatCast { get; set; }
+        public string Producer { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
